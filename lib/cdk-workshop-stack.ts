d44607a25ec64aeafe1006da86d294e0fd28cb2d
voideus <bikash.tamang@wesionary.team>
2023-12-07 17:40:09 +0545

    cdk deployment basics

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,11 +1,24 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import {
+  Duration,
+  Stack,
+  StackProps,
+  lambda_layer_node_proxy_agent,
+} from "aws-cdk-lib";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, "Hello Handler", {
+      runtime: lambda.Runtime.NODEJS_16_X, // execution environment
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler",
+    });
   }
 }
